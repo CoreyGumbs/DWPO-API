e39f58afdd9783aada9c20cd6d19352afac4c796
Sonoflight <coreygumbs@gmail.com>
2016-02-24 21:03:10 -0500

    Added basic API route and git test

---
+++ src/server.js
@@ -0,0 +1,17 @@
+var express = require('express');  
+var body_parser = require('body-parser'); 
+var app = express();   
+
+//Config
+var port = 3000; 
+
+app.use(body_parser.json());
+
+//routes
+app.use('/api', require('../routes/api.js')(express));
+
+var server = app.listen(port, function(){
+	console.log('Server Active on ', port);
+});
+
+module.exports = server;
